01f4d0ffac291794efd4a779e5e4aefffb554a82
Stefani Natalia <stefani@luxodev.com>
2018-10-27 13:45:28 +0700

    increment

--- lib/spree_print_invoice/version.rb
+++ lib/spree_print_invoice/version.rb
@@ -10,7 +10,7 @@ module SpreePrintInvoice
   module VERSION
     MAJOR = 3
     MINOR = 0
-    TINY  = 0
+    TINY  = 1
     PRE   = 'beta'
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
